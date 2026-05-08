a1b86a4ec9b2a4af4e912fdb6011a7d9b8f68f4e
Alex <blackalex1@users.noreply.github.com>
2026-05-08 18:21:15 +0300

    Enhance rebuild.sh: auto-regenerate nginx.conf from template

--- deploy/rebuild.sh
+++ deploy/rebuild.sh
@@ -13,6 +13,17 @@ git pull
 
 # 2. Rebuild and restart only the app container
 echo "### Rebuilding and restarting application..."
+# Regenerate nginx.conf from template before building
+if [ -f "../.env" ]; then
+    export $(grep -v '^#' ../.env | xargs)
+    if [ -f "./nginx.conf.template" ]; then
+        echo "### Regenerating Nginx config from template..."
+        cp ./nginx.conf.template ./nginx.conf
+        sed -i "s/{{DOMAIN_NAME}}/${DOMAIN_NAME}/g" ./nginx.conf
+        sed -i "s/{{HTTPS_PORT}}/${HTTPS_PORT:-443}/g" ./nginx.conf
+    fi
+fi
+
 docker compose --env-file ../.env up -d --build markflow
 
 # 3. Reload Nginx just in case
